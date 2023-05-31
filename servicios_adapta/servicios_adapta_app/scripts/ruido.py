00d2b003fb2534306f0e8424dd9fdcb5ef6df963
Agustin Moras <a.morascastro@gmail.com>
2023-05-31 15:50:39 -0300

    numbers as strings solved

--- servicios_adapta/servicios_adapta_app/scripts/ruido.py
+++ servicios_adapta/servicios_adapta_app/scripts/ruido.py
@@ -14,7 +14,8 @@ def csv_df(path):
     nombres_columnas = df.iloc[0]
     if nombres_columnas[0] == 'Fecha':
         df.columns = nombres_columnas
-        df = df[1:].reset_index()
+        df = df[1:].reset_index(drop=True)
+        df[df.columns[2]] = df[df.columns[2]].astype(float)
     else:
         nombres_columnas = ['Fecha', 'Tiempo', 'Laeq']
         df.columns = nombres_columnas
@@ -65,6 +66,7 @@ def create_analysis(csv, template_ws,mins, ef):
     # template_ws = temp[temp.sheetnames[1]]
 
     data = csv_df(csv)
+    print(data.head())
     name_code, date_code = name_code_and_date(csv.name)
 
     #columns
