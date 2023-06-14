676c72d9c5140181490b56c498a9942a1a75e811
Agustin <a.morascastro@gmail.com>
2023-06-14 17:34:05 -0300

    admin.py changes

--- servicios_adapta/servicios_adapta_app/admin.py
+++ servicios_adapta/servicios_adapta_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from servicios_adapta_app.models import Proyecto, Punto, Medicion
+from servicios_adapta_app.models import Proyecto, Punto, Medicion, experiencia_Categoria, experiencia_Cliente, experiencia_Contacto, experiencia_Contrato, experiencia_Localizacion, experiencia_Proyecto
 
 admin.site.register(Proyecto)
 admin.site.register(Punto)
 admin.site.register(Medicion)
+admin.site.register(experiencia_Cliente)
+admin.site.register(experiencia_Contacto)
+admin.site.register(experiencia_Categoria)
+admin.site.register(experiencia_Localizacion)
+admin.site.register(experiencia_Proyecto)
+admin.site.register(experiencia_Contrato)
