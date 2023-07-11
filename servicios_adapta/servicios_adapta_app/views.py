5ca8476b84e79882b54a8b6cc1a507acd4ed65a2
Agustin Moras <a.morascastro@gmail.com>
2023-07-11 17:15:25 -0300

    razones views

--- servicios_adapta/servicios_adapta_app/views.py
+++ servicios_adapta/servicios_adapta_app/views.py
@@ -13,6 +13,7 @@ from django.conf import settings
 from .models import Medicion, Punto
 from datetime import datetime
 from django.contrib.sessions.models import Session
+from .models import experiencia_Cliente
 import zipfile
 
 # Create your views here.
@@ -306,6 +307,22 @@ def eliminarFracciones(tiempo):
 
 def experienciaRazones(request):
     if request.user.is_authenticated:
-        pass
+        if request.method == 'POST':
+            nombre_razon = request.POST.get('nombre-razon')
+            
+            # Verificar si ya existe una razón social con el mismo nombre
+            if experiencia_Cliente.objects.filter(name=nombre_razon).exists():
+                error_message = "Ya existe una razón social con ese nombre."
+            else:
+                # Crear una nueva instancia de experiencia_Cliente con el nombre proporcionado
+                experiencia_Cliente.objects.create(name=nombre_razon)
+                return redirect('experienciaRazones')
+
+        else:
+            error_message = ""
+
+        razones = experiencia_Cliente.objects.all()
+        context = {'razones': razones, 'error_message': error_message}
+        return render(request, './servicios_adapta_app/experiencia_razones.html', context)
     else:
         return redirect('login')
